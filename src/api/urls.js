85a60e3706d620076378dc9aa24dd0d9ebd233f6
nisarg-78 <gajjarnisarg78@gmail.com>
2023-01-10 15:41:33 +0530

    urls.js updated

--- src/api/urls.js
+++ src/api/urls.js
@@ -1,3 +1,8 @@
-const apiUrl = 'http://localhost:8000/api';
-// const apiUrl = 'https://ark-api-r8tb.onrender.com/api';
-export { apiUrl };
+console.log(process.env.NODE_ENV)
+
+if (process.env.NODE_ENV === "development") {
+	const apiUrl = "http://localhost:8000/api"
+} else {
+	const apiUrl = "https://ark-api-r8tb.onrender.com/api"
+}
+export { apiUrl }
